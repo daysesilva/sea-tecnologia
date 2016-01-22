b3f55099733046a563127abf0ba7f384c1693eb1
Dayse Silva Costa <dayse.dsc@gmail.com>
2016-01-22 01:29:06 +0000

    Create quase pronto.

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
 app = Flask(__name__)
+engine = create_engine('sqlite:///:memory:', convert_unicode=True)
+db_session = scoped_session(
+    sessionmaker(
+        autocommit=True,
+        autoflush=False,
+        bind=engine
+    )
+)
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+class Language(Base):
+    __tablename__ = 'language'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    
+    def __init__(self, name):
+        self.name = name
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    languages = Language.query.all()
+    return render_template('index.html', languages=languages)
+    
+@app.route('/create', methods=['POST'])
+def create():
+    language = Language(name=request.form['name'])
+    db_session.add(language)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    Base.metadata.create_all(bind=engine)
     app.run(host='0.0.0.0')
